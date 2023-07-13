c399c9db2781573a0d8803a6478370d8027e2302
teefa28 <owusuah28@gmail.com>
2023-07-13 09:18:58 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	pintf("Size of a char: %d byte(s)\n", sizeof(char));
-	pintf("Size of a int: %d byte(s)\n", sizeof(int));
+	pintf("Size of an int: %d byte(s)\n", sizeof(int));
 	pintf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	pintf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	pintf("Size of a float: %d byte(s)\n", sizeof(float));
