c56cce955ba9aabdb5f876530fa4b01e6496a0b1
teefa28 <owusuah28@gmail.com>
2023-07-13 09:15:14 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	pintf("Size of a char: %d byte(s)\n", sizeof(char));
+	pintf("Size of a int: %d byte(s)\n", sizeof(int));
+	pintf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	pintf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	pintf("Size of a float: %d byte(s)\n", sizeof(float));
+}
