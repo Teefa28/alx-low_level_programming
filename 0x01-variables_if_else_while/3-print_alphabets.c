8c1b6124ae730cafee875ef03815cafd56640ec4
teefa28 <owusuah28@gmail.com>
2023-07-14 19:18:47 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+
 /**
-* main - print alphabets
-* Return: always 0 (success)
-*/
+ *  main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char lowercase[] = "abcdefghijklmnopqrstuvwxyz"
-putchar("%s\n", lowercase);
-char uppercase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-putchar("%s\n", uppercase);
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+for (i = 0; i < 52; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
 return (0);
 }
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/**
+* main - print alphabets
+* Return: always 0 (success)
+*/
+int main(void)
+{
