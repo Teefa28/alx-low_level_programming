7f2a8480cd7eeee568c7c11838c239b893b03e87
teefa28 <owusuah28@gmail.com>
2023-07-14 18:08:51 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print alphabets
+* Return: always 0 (success)
+*/
+int main(void)
+{
+char lowercase[] = "abcdefghijklmnopqrstuvwxyz"
+putchar("%s\n", lowercase);
+char uppercase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+putchar("%s\n", uppercase);
+return (0);
+}
