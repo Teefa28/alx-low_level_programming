e844b2b3eb5fbd033c07da964e099c5d22bd0fe6
teefa28 <owusuah28@gmail.com>
2023-07-20 12:43:54 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,13 @@ void more_numbers(void)
 int a, b;
 
 for (a = 0; a < 10; a++)
+{
 for (b = 0; b < 15; b++)
 {
-if (a >= 10)
-_putchar (b / 10 + '0');
-_putchar (b % 10 + '0');
+if (b >= 10)
+_putchar(b / 10 + '0');
+_putchar(b % 10 + '0');
 }
 _putchar('\n');
+}
 }
